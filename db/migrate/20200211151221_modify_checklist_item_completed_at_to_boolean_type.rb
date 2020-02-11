296801f939fc80ea0c663323b4e45eadb1a3fca5
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-11 18:05:00 +0200

    postgres

--- db/migrate/20200211151221_modify_checklist_item_completed_at_to_boolean_type.rb
+++ db/migrate/20200211151221_modify_checklist_item_completed_at_to_boolean_type.rb
@@ -1,6 +1,6 @@
 class ModifyChecklistItemCompletedAtToBooleanType < ActiveRecord::Migration[6.0]
   def change
-    change_column :checklist_items, :completed_at, :boolean
+    change_column :checklist_items, :completed_at, "boolean USING completed_at::boolean"
     rename_column :checklist_items, :completed_at, :completed
   end
 end
