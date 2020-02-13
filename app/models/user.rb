69c580f686529dd6ad37d8c1fefd95f3d75a45bd
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-13 09:57:46 +0200

    refactor: update user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates :email, uniqueness: true
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
 end
