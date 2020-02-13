4eeba156bc8a973855b9d6d3d44aed135dec4e8d
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-13 10:01:25 +0200

    refactor: update user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ApplicationRecord
   validates_presence_of :email
   validates :email, uniqueness: true
   validates_presence_of :password
-  validates_presence_of :password_confirmation
 end
