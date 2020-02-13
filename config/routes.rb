d19f0efb953010509d95f2f69b9d6fcdd47c0662
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-13 09:47:05 +0200

    chore: set up devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get "sign_in", to: "devise/sessions#new"
+  end
+
   resources :checklists do
     resources :checklist_items do
       member do
