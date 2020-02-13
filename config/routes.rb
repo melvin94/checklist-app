642448075057dc27982d72b1ef764105f9e3b6a5
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-13 14:00:30 +0200

    fix: change root depending on user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions]
+  devise_for :users, skip: [:sessions], controllers: {
+                       passwords: "devise/passwords",
+                       registrations: "devise/registrations",
+                       sessions: "devise/sessions",
+                       confirmations: "devise/confirmations",
+                     }
   resources :users
 
   devise_scope :user do
     get "login", to: "devise/sessions#new"
     post "login", to: "devise/sessions#create"
     delete "logout", to: "devise/sessions#destroy"
+
+    authenticated :user do
+      root "users#index", as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root to: redirect("/login"), as: :unauthenticated_root
+    end
   end
 
   resources :checklists do
@@ -15,6 +28,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  root to: redirect("/login")
 end
