ad4b3563def169238f96e1732677e9c259b0f0d6
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-13 11:40:15 +0200

    feat: add user CRUD functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,46 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    # TODO: this action
   end
 
   private
 
   def user_params
-    params.permit(:user).require(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_prefix: "devise"
+  resources :users
 
   devise_scope :user do
     get "log_in", to: "devise/sessions#new"
