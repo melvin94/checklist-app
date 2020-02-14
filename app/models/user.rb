01189bcec7f366ed50cff2d4d714e765dabd7139
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-14 12:31:11 +0200

    refactor: update user to include role

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name
   validates_presence_of :last_name
+  validates_presence_of :role_id
   validates_presence_of :email
   validates :email, uniqueness: true
   validates_presence_of :password
