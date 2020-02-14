03d94b7c22ff60604629201e1b2e02bb512ad8be
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-14 15:18:23 +0200

    chore: add default database seed for platformgst

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+support_role = Role.new(title: "Tech-Support", admin: true)
+support_role.save!
+
+tech_support_user = User.new(first_name: "Tech", last_name: "Support", email: "shrimp-ops-ts@platform45.com", password: "123456", password_confirmation: "123456", role_id: "1")
+tech_support_user.save!
