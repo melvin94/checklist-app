96586b3ed7c26d0cf43a76951a08e451c4f63628
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-14 14:46:35 +0200

    refactor: update root path on Pundit Authorization error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || root_path)
+    redirect_to(request.referrer || authenticated_root_path)
   end
 end
