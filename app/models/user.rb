b1578ab0410e88ab3f5d8b61abb8243b39b4a483
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-27 15:15:49 +0200

    refactor: user belongs to access level and role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable
 
+  belongs_to :access_level
+  belongs_to :role
+
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :role_id
