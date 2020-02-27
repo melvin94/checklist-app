d2cef1d8d29a769fdf110c7b0c5f45bcb6b0890f
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-02-27 15:18:29 +0200

    refactor: add role to user model

--- db/migrate/20200214101515_add_role_to_users.rb
+++ db/migrate/20200214101515_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_reference :users, :role, foreign_key: true
+    add_reference :users, :role, index: true, foreign_key: true
   end
 end
