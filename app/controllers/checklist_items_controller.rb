3e770eb360396d710852b69aaf1497a700cd9046
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-03-09 16:53:10 +0200

    WIP: adjust checklist item complete status boolean

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -32,6 +32,13 @@ class ChecklistItemsController < ApplicationController
 
   def complete
     @checklist_item.update_attribute(:completed, !@checklist_item.completed)
+    if @checklist_item.completed
+      if @checklist_item.result.nil?
+        @checklist_item.update_attribute(:completed, !@checklist_item.completed)
+      end
+    elsif !@checklist_item.completed
+      @checklist_item.update_attribute(:result, nil)
+    end
     redirect_to @checklist, notice: "Checklist item completed"
   end
 
@@ -46,6 +53,10 @@ class ChecklistItemsController < ApplicationController
   end
 
   def checklist_item_params
-    params[:checklist_item].permit(:content)
+    if params[:checklist_item]
+      params[:checklist_item].permit(:description, :type)
+    else
+      { description: @checklist_item.description, type: @checklist_item.type, result: params[:result], completed: true }
+    end
   end
 end
