b077b7477181b1f1feb6f4c91e368cad90c1350e
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-03-11 16:23:26 +0200

    chore: add active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_082340) do
+ActiveRecord::Schema.define(version: 2020_03_11_142038) do
 
   create_table "access_levels", force: :cascade do |t|
     t.integer "level"
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "checklist_items", force: :cascade do |t|
@@ -70,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_082340) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "checklist_items", "checklists"
   add_foreign_key "users", "access_levels"
   add_foreign_key "users", "roles"
