e07004b6cf6f32d7218d0ae0412894d6d913b4d4
Melvin Mathew <melvin.julian.mathew@gmail.com>
2020-03-20 10:24:23 +0200

    refactor: correct image validation

--- app/models/checklist_item_image.rb
+++ app/models/checklist_item_image.rb
@@ -1,3 +1,12 @@
 class ChecklistItemImage < ChecklistItem
   has_one_attached :image
+
+  validate :not_blank_attachment
+
+  def not_blank_attachment
+    if image.blank?
+      image.purge # delete the uploaded file
+      errors.add(:image, "needs to be selected before upload")
+    end
+  end
 end
